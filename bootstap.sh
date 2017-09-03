3a0ee1e8d6dc72d05f78c3a6cf270cfc5301f820
Felix Kreuk <felixkreuk@gmail.com>
2017-09-03 21:21:34 +0300

    update

--- bootstap.sh
+++ bootstap.sh
@@ -1,29 +1,18 @@
 #!/usr/bin/env bash
-
-cd "$(dirname "${BASH_SOURCE}")";
-
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 git pull origin master;
 
 function doIt() {
-	rsync
-    --exclude ".git/" \
+	rsync --exclude ".git/" \
 		--exclude "bootstrap.sh" \
     --exclude ".gitignore" \
     --exclude "brew_apps.txt" \
     --exclude "config" \
     --exclude "dot_backup.sh" \
 		-avh --no-perms . ~;
-  cp ./config ~/.ssh/config
+  cp ./config ~/.ssh/config;
 	source ~/.zshrc;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
+doIt
 unset doIt;
