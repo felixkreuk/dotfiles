0eed00b121ff1de23527e2c7a4f6711397572f4a
Felix Kreuk <felixkreuk@gmail.com>
2017-09-03 22:11:45 +0300

    update

--- dot_backup.sh
+++ dot_backup.sh
@@ -1,6 +1,11 @@
-echo "Updating dot files..."
+echo " -----------------------------------------"
+echo "| Updating local dot files to remote...   |"
+echo " -----------------------------------------"
+echo "Updating local dot files to remote..."
 cd ~/Workspace/dotfiles
 git add .
 git commit -m "update"
 git push origin master
-echo "Done."
+echo " -------------------------------"
+echo "| Done.                         |"
+echo " -------------------------------"
