3201469f4305d55f65fd46a9dbcf0c56099016eb
Felix Kreuk <felixkreuk@gmail.com>
2017-10-28 21:03:02 +0300

    update

--- dot_backup.sh
+++ dot_backup.sh
@@ -1,12 +1,13 @@
+echo "Copying local files to repo..."
 HERE=~/Workspace/dotfiles/
 cp ~/.zsh_config $HERE
-echo " -----------------------------------------"
-echo "| Updating local dot files to remote...   |"
-echo " -----------------------------------------"
+cp ~/.aliases $HERE
+cp ~/.zshrc $HERE
+cp ~/.tmux.conf $HERE
+cp ~/.ssh/config $HERE
+echo "Updating local dot files to remote..."
 cd ~/Workspace/dotfiles
 git add .
 git commit -m "update"
 git push origin master
-echo " -----------------------------------------"
-echo "| Done.                                   |"
-echo " -----------------------------------------"
+echo "Done."
