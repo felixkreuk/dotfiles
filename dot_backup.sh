aa60f530f35fcc200171d03d3449fb9cb69bc65c
Felix Kreuk <felixkreuk@gmail.com>
2018-02-18 19:16:53 +0200

    update

--- dot_backup.sh
+++ dot_backup.sh
@@ -1,5 +1,5 @@
 echo "Copying local files to repo..."
-HERE=~/Workspace/dotfiles/
+HERE=~/Workspace/code/dotfiles/
 cp ~/.zsh_config $HERE
 cp ~/.functions $HERE
 cp ~/.aliases $HERE
@@ -10,7 +10,7 @@ cp ~/.config/nvim/init.vim $HERE
 cp ~/.gitconfig $HERE
 cp ~/.gitignore $HERE
 echo "Updating local dot files to remote..."
-cd ~/Workspace/dotfiles
+cd $HERE
 git add .
 git commit -m "update"
 git push origin master
