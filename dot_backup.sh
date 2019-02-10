59c6250a611f9db0b87f020e942bbd9e4e12bba0
Felix Kreuk <felixkreuk@gmail.com>
2019-02-10 23:44:04 +0200

    update

--- dot_backup.sh
+++ dot_backup.sh
@@ -2,21 +2,21 @@
 
 echo "Copying local files to repo..."
 HERE=~/Workspace/code/dotfiles/
-cp ~/.zsh_config $HERE
-cp ~/.functions $HERE
-cp ~/.aliases $HERE
-cp ~/.zshrc $HERE
-cp ~/.tmux.conf $HERE
-cp ~/.ssh/config $HERE
-cp ~/.config/nvim/init.vim $HERE
-cp ~/.gitconfig $HERE
-cp ~/.gitignore $HERE
-cp ~/.vim_runtime/my_configs.vim $HERE
-cp ~/.config/nvim/init.vim $HERE
-cp ~/.config/nvim/colors/ $HERE
-cp ~/.vim/plugged/neosnippet-snippets/neosnippets/python.snip $HERE
-cp ~/.config/fish/config.fish $HERE
-cp ~/.config/fish/fishfile $HERE
+cp -r ~/.zsh_config $HERE
+cp -r ~/.functions $HERE
+cp -r ~/.aliases $HERE
+cp -r ~/.zshrc $HERE
+cp -r ~/.tmux.conf $HERE
+cp -r ~/.ssh/config $HERE
+cp -r ~/.config/nvim/init.vim $HERE
+cp -r ~/.gitconfig $HERE
+cp -r ~/.gitignore $HERE
+cp -r ~/.vim_runtime/my_configs.vim $HERE
+cp -r ~/.config/nvim/init.vim $HERE
+cp -r ~/.config/nvim/colors/ $HERE
+cp -r ~/.vim/plugged/neosnippet-snippets/neosnippets/python.snip $HERE
+cp -r ~/.config/fish/config.fish $HERE
+cp -r ~/.config/fish/fishfile $HERE
 echo "Updating local dot files to remote..."
 cd $HERE
 git add .
