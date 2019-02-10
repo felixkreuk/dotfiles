f2d0a770cbe2e0c02867232f36c21fcb6025a047
Felix Kreuk <felixkreuk@gmail.com>
2019-02-10 23:52:29 +0200

    update

--- dot_backup.sh
+++ dot_backup.sh
@@ -9,7 +9,6 @@ cp -r ~/.zshrc $HERE
 cp -r ~/.tmux.conf $HERE
 cp -r ~/.ssh/config $HERE
 cp -r ~/.gitconfig $HERE
-cp -r ~/.gitignore $HERE
 cp -r ~/.vim_runtime/my_configs.vim $HERE
 cp -r ~/.config/nvim/
 cp -r ~/.vim/plugged/neosnippet-snippets/neosnippets/python.snip $HERE
