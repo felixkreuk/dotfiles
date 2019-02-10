424660cacce9450948131d11132246918530ba87
Felix Kreuk <felixkreuk@gmail.com>
2019-02-10 23:52:05 +0200

    update

--- dot_backup.sh
+++ dot_backup.sh
@@ -8,15 +8,12 @@ cp -r ~/.aliases $HERE
 cp -r ~/.zshrc $HERE
 cp -r ~/.tmux.conf $HERE
 cp -r ~/.ssh/config $HERE
-cp -r ~/.config/nvim/init.vim $HERE
 cp -r ~/.gitconfig $HERE
 cp -r ~/.gitignore $HERE
 cp -r ~/.vim_runtime/my_configs.vim $HERE
-cp -r ~/.config/nvim/init.vim $HERE
-cp -r ~/.config/nvim/colors/ $HERE
+cp -r ~/.config/nvim/
 cp -r ~/.vim/plugged/neosnippet-snippets/neosnippets/python.snip $HERE
-cp -r ~/.config/fish/config.fish $HERE
-cp -r ~/.config/fish/fishfile $HERE
+cp -r ~/.config/fish/
 echo "Updating local dot files to remote..."
 cd $HERE
 git add .
