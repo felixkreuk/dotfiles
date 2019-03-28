ad7e15adba469a045373629cad91c1471f16b6b3
5697095896 <felixkreuk@Felixs-MacBook-Pro.local>
2019-03-28 15:27:56 +0200

    update

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # instsall syntax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+chsh -s "$(command -v zsh)"
 
 echo "==> symlinking"
 if [ ! -d "$HOME/.config" ]; then
