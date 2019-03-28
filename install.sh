43aa20e82393abba0140351c41189d3112f14309
5697095896 <felixkreuk@Felixs-MacBook-Pro.local>
2019-03-28 14:16:36 +0200

    update

--- install.sh
+++ install.sh
@@ -1,22 +1,25 @@
 # clone my dot files and copy to relevant spots
 echo "==> downloading dotfiles"
-cd ~; mkdir installations; cd installations
-git clone https://github.com/felixkreuk/dotfiles.git
-cd dotfiles
+cd $HOME
+git clone https://github.com/felixkreuk/dotfiles.git .dotfiles
+cd .dotfiles
 
 echo "==> symlinking"
 if [ ! -d "$HOME/.config" ]; then
     echo "==> creating ~/.config"
     mkdir -p "$HOME/.config"
 fi
-ln -s `pwd`/.config/nvim ~/.config/nvim
-ln -s `pwd`/.config/nvim/init.vim ~/.vimrc
-ln -s `pwd`/zsh/zshrc.symlink ~/.zshrc
-ln -s `pwd`/tmux/tmux.conf.symlink ~/.tmux.conf
+ln -s $HOME/.dotfiles/.config/nvim ~/.config/nvim
+ln -s $HOME/.dotfiles/.config/nvim/init.vim ~/.vimrc
+ln -s $HOME/.dotfiles/zsh/zshrc.symlink ~/.zshrc
+ln -s $HOME/.dotfiles/tmux/tmux.conf.symlink ~/.tmux.conf
 
 echo "==> intsalling neovim python support"
 pip2 install --user neovim
 pip3 install --user neovim
+
+echo "==> installing zplug"
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 echo "==> installing vim-plug"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
