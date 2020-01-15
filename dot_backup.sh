9364d9436eaaa3109ae6da1017abed7dccccc258
felixkreuk <hqetsr+11pmi3emu97g3rganbwgf5pa@teleosaurs.xyz>
2020-01-15 11:18:59 +0200

    update

--- dot_backup.sh
+++ dot_backup.sh
@@ -2,16 +2,11 @@
 
 echo "Copying local files to repo..."
 HERE=~/Workspace/code/dotfiles/
-cp -r ~/.zsh_config $HERE
-cp -r ~/.functions $HERE
-cp -r ~/.aliases $HERE
-cp -r ~/.zshrc $HERE
 cp -r ~/.tmux.conf $HERE
 cp -r ~/.ssh/config $HERE/.ssh/
 cp -r ~/.gitconfig $HERE
 cp -r ~/.vim_runtime/my_configs.vim $HERE
 cp -r ~/.config/nvim/ $HERE/.config/nvim
-cp -r ~/.config/fish/ $HERE/.config/fish
 cp -r ~/.vim/plugged/neosnippet-snippets/neosnippets/python.snip $HERE
 echo "Updating local dot files to remote..."
 cd $HERE
