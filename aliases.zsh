594446ec8b13c42c35cea54fd1dca4ba44ea4ace
felixkreuk <hqetsr+11pmi3emu97g3rganbwgf5pa@teleosaurs.xyz>
2020-01-17 22:41:27 +0200

    1

--- aliases.zsh
+++ aliases.zsh
@@ -30,13 +30,21 @@ alias pgrep='ps aux | grep'
 alias tmux='tmux -2'
 alias tt='tmux -2 a'
 
+# clean finished
 alias tsc='for GPU in 0 1 2 3; do TS_SOCKET=/tmp/felix_gpu_$GPU ts -C; done'
+# display all queues
 alias tss='for GPU in 0 1 2 3; do TS_SOCKET=/tmp/felix_gpu_$GPU ts; done'
+# display all running/ququed/finished
+alias tsr='for GPU in 0 1 2 3; do echo "gpu $GPU"; TS_SOCKET=/tmp/felix_gpu_$GPU ts | grep running; done'
+alias tsq='for GPU in 0 1 2 3; do echo "gpu $GPU"; TS_SOCKET=/tmp/felix_gpu_$GPU ts | grep queued; done'
+alias tsf='for GPU in 0 1 2 3; do echo "gpu $GPU"; TS_SOCKET=/tmp/felix_gpu_$GPU ts | grep finished; done'
+# display queue by number
 alias ts0='TS_SOCKET=/tmp/felix_gpu_0 ts'
 alias ts1='TS_SOCKET=/tmp/felix_gpu_1 ts'
 alias ts2='TS_SOCKET=/tmp/felix_gpu_2 ts'
 alias ts3='TS_SOCKET=/tmp/felix_gpu_3 ts'
 
+# kill according to substring
 kill_ts () {
 	for GPU in 0 1 2 3
 	do
@@ -54,7 +62,7 @@ kill_ts () {
 	done
 }
 
-
+# display general running/queued/finished stats
 function tsl() { 
 	echo "running:  $(tss | grep running | tr -s " " | cut -c 1-10 | wc -l)" 
 	echo "queued:   $(tss | grep queued | tr -s " " | cut -c 1-10 | wc -l)" 
