3a392f022815393df841e81384c6bf44c638afc4
Felix Kreuk <felixkreuk@Felixs-MacBook-Pro.local>
2020-09-29 20:14:15 +0300

    update brew

--- brew.sh
+++ brew.sh
@@ -4,39 +4,34 @@ brew upgrade
 brew tap caskroom/cask
 
 brew install neovim
-brew install fzf && (brew --prefix)/opt/fzf/install
+brew install fzf && $(brew --prefix)/opt/fzf/install
 brew install htop
 brew install pandoc
 brew install tmux
 brew install zsh
 
-brew cask install dozer
 brew cask install slack
 brew cask install whatsapp
-brew cask install messenger
 brew cask install praat
 brew cask install atext
 brew cask install notion
 brew cask install alfred
 brew cask install rectangle
-brew cask install keka
 brew cask install transmission
-brew cask install itsycal
-brew cask install texpad
-brew cask install timing
 brew cask install keyboardcleantool
 brew cask install iterm2
 brew cask install bitwarden
 brew cask install google-backup-and-sync
-brew cask install font-fira-code
 brew cask install spotify
 brew cask install iina
 brew cask install visual-studio-code
 brew cask install marta
-brew cask install dozer
 brew cask install hazel
 brew cask install bettertouchtool
 brew cask install jupyter-notebook-viewer
+
+brew tap homebrew/cask-fonts
+brew cask install font-fira-code
 
 brew cleanup
 
