31daa7c1ab364c4209a9a95484a7e4ddd7be261f
Felix Kreuk <felixkreuk@gmail.com>
2021-05-06 12:44:17 +0300

    update

--- brew.sh
+++ brew.sh
@@ -14,9 +14,7 @@ brew cask install slack
 brew cask install whatsapp
 brew cask install praat
 brew cask install atext
-brew cask install notion
 brew cask install alfred
-brew cask install rectangle
 brew cask install transmission
 brew cask install keyboardcleantool
 brew cask install iterm2
@@ -26,7 +24,6 @@ brew cask install google-backup-and-sync
 brew cask install spotify
 brew cask install iina
 brew cask install visual-studio-code
-brew cask install marta
 brew cask install hazel
 brew cask install bettertouchtool
 brew cask install jupyter-notebook-viewer
